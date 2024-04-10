77450e5eb53cfaf63a6706fa3e928143c0e01168
Eugeneowner <ownereugene@gmail.com>
2024-04-10 22:49:05 +0300

    Add Ukr language

--- src/Components/Services/seviceItem.jsx
+++ src/Components/Services/seviceItem.jsx
@@ -4,9 +4,9 @@ const ServiceItem = ({price,weight}) => {
   return ( 
     <li className={s.services_list_item}>
       <BoxIcon/>
-      <h3 className={s.services_list_item_weight}>Up to {weight} kg</h3>
+      <h3 className={s.services_list_item_weight}>до {weight} кг</h3>
       <h2 className={s.services_list_item_price}>{price} PLN</h2>
-    <button className={s.services_list_item_button}>Choose</button>
+    <button className={s.services_list_item_button}>Обрати</button>
     </li>
   )
 }
