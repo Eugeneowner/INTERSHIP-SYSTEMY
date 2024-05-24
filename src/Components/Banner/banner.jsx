3f5b7b2bf7b4db7669668350facdba9d44ba7419
abegma <abegma137@gmail.com>
2024-05-24 19:51:28 +0300

    fix banner

--- src/Components/Banner/banner.jsx
+++ src/Components/Banner/banner.jsx
@@ -2,7 +2,7 @@ import { AnchorLink } from "react-anchor-navigation";
 import Header from "../Header/header";
 import s from "./banner.module.scss";
 
-import {t} from "i18next"
+import { t } from "i18next";
 
 const Banner = () => {
   return (
@@ -10,10 +10,11 @@ const Banner = () => {
       <Header />
       <h1 className={s.banner_name}>INTERSHIP SYSTEMY</h1>
       <h2 className={s.banner_description}>{t("banner.description")}</h2>
-      <h3 className={s.banner_description_mission}>{t("banner.mission")}</h3>
       <AnchorLink className={s.link} href="#sevices">
-      <button className={s.banner_button}>{t("banner.button")}</button>
+        <button className={s.banner_button}>{t("banner.button")}</button>
       </AnchorLink>
+      <h3 className={s.banner_description_mission}>{t("banner.mission")}</h3>
+      
     </section>
   );
 };
